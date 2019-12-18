938fc5028b9ec522d0b6b2c64bec5618851eaea7
Zhao, Wei <frank.zhao@siemens.com>
2019-12-18 12:34:51 +0800

    <

--- init.el
+++ init.el
@@ -1,28 +1,5 @@
-(require 'cl)
-(require 'package)
-(setq package-enable-at-startup nil)
-
-
-
-(setq package-archives '(("gnu-qinghua"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("melpa-qinghua" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-			 ("org-qinghua"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
-
-(setq my-emacs-root-directory "~/.emacs.d/")
-(add-to-list 'load-path (expand-file-name "lisp" my-emacs-root-directory))
-(require 'init-benchmarking) ;; Measure startup time
 (load "~/.emacs.d/my-init.el")
 
-(package-initialize)
-
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-
-;; Use my init file in org-mode to set other packages
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 ;; ;; Use a hook so the message doesn't get clobbered by other messages.
 ;; (add-hook 'emacs-startup-hook
